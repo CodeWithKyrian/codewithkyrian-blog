0d7c7bfe0c48f2e830e3688ed321db8fe150782d
Kyrian Obikwelu <koshnawaza@gmail.com>
2025-03-06 09:12:50 +0100

    feat: Prepare for render deployment

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Running seeder..."
+php artisan db:seed
+
+echo "Optimizing Filament..."
+php artisan filament:optimize
+
+echo "Generate API docs.."
+php artisan scribe:generate
